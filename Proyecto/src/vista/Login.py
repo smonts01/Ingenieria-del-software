8e3ba68d883a07bada4cd01eff3c3ef9e2955bf2
smonts01 <smonts01@estudiantes.unileon.es>
2026-04-27 18:11:47 +0200

    Create Login.py
    Login hecho

---
+++ Proyecto/src/vista/Login.py
@@ -0,0 +1,63 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QLineEdit
+from PyQt5 import uic
+
+# Cargar la interfaz generada desde el archivo .ui
+Form, Window = uic.loadUiType("./src/vista/Ui/Login.ui")
+
+
+class MiVentana(QMainWindow, Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  # Inicializa los widgets
+
+        # Conectar botones
+        self.pushButton.clicked.connect(self.iniciar_sesion)
+        self.btnOjo.clicked.connect(self.mostrar_ocultar_contrasena)
+
+        # Estado inicial: contraseña oculta + ojo abierto
+        self.txtContrasea.setEchoMode(QLineEdit.Password)
+        self.btnOjo.setStyleSheet("""
+        QToolButton {
+            image: url(./src/vista/imagenes/ojo_abierto.png);
+            background: transparent;
+            border: none;
+        }
+        """)
+
+    def iniciar_sesion(self):
+        usuario = self.txtUsuario.text()
+        contrasena = self.txtContrasea.text()
+
+        if usuario == "" or contrasena == "":
+            QMessageBox.warning(self, "Error", "Debes completar usuario y contraseña")
+        else:
+            QMessageBox.information(self, "Login", "Datos introducidos correctamente")
+
+    def mostrar_ocultar_contrasena(self):
+        if self.txtContrasea.echoMode() == QLineEdit.Password:
+            # Mostrar contraseña + ojo cerrado
+            self.txtContrasea.setEchoMode(QLineEdit.Normal)
+            self.btnOjo.setStyleSheet("""
+            QToolButton {
+                image: url(./src/vista/imagenes/ojo_cerrado.png);
+                background: transparent;
+                border: none;
+            }
+            """)
+        else:
+            # Ocultar contraseña + ojo abierto
+            self.txtContrasea.setEchoMode(QLineEdit.Password)
+            self.btnOjo.setStyleSheet("""
+            QToolButton {
+                image: url(./src/vista/imagenes/ojo_abierto.png);
+                background: transparent;
+                border: none;
+            }
+            """)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    ventana = MiVentana()
+    ventana.show()
+    app.exec_()
