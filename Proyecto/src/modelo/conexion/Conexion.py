aba80f24b290e2d58733b7dab29ffd0a4c10b8c0
Lidia <lmerim00@estudiantes.unileon.es>
2026-05-02 18:30:08 +0200

    Create Conexion.py

---
+++ Proyecto/src/modelo/conexion/Conexion.py
@@ -0,0 +1,40 @@
+import jaydebeapi
+
+class Conexion:
+    def __init__(self, host='localhost', database='Stayfit_database', user='root', password='pruebaISD2024'):
+        self._host = host
+        self._database = database
+        self._user = user
+        self._password = password
+        self.conexion = self.createConnection()
+
+    def createConnection(self):
+        try:
+            jdbc_driver = "com.mysql.cj.jdbc.Driver"
+            jar_file = "lib/mysql-connector-j-8.3.0.jar"
+            self.conexion = jaydebeapi.connect(
+                jdbc_driver,
+                f"jdbc:mysql://{self._host}/{self._database}",
+                [self._user, self._password],
+                jar_file
+            )
+            return self.conexion
+        except Exception as e:
+            print("Error creando conexión:", e)
+            return None
+
+    """Un cursor es una estructura de control que permite recorrer los resultados de una 
+    consulta SQL y manipular fila por fila los datos recuperados desde una base de datos."""
+    def getCursor(self):
+        if self.conexion is None:
+            self.createConnection()
+        return self.conexion.cursor()
+
+    def closeConnection(self):
+        try:
+            if self.conexion:
+                self.conexion.close()
+                self.conexion = None
+        except Exception as e:
+            print("Error cerrando conexión:", e)
+
