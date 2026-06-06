f11930edfd76cfc949f413bc772ce88b72104d6b
smonts01 <smonts01@estudiantes.unileon.es>
2026-06-06 19:16:16 +0200

    cambios

--- Proyecto/src/modelo/VO/EntrenadorVO.py
+++ Proyecto/src/modelo/VO/EntrenadorVO.py
@@ -1,17 +1,4 @@
 class EntrenadorVO:
-    def __init__(self, id_entrenador, especialidad, id_administrador_registra):
-        self._id_entrenador = id_entrenador
-        self._especialidad = especialidad
-        self._id_administrador_registra = id_administrador_registra
-        
-    @property
-    def id_entrenador(self):
-        return self._id_entrenador
-    
-    @property
-    def especialidad(self):
-        return self._especialidad
-    
-    @property
-    def id_administrador_registra(self):
-        return self._id_administrador_registra
+    def __init__(self, id_entrenador=None, id_administrador_registra=None):
+        self.id_entrenador = id_entrenador
+        self.id_administrador_registra = id_administrador_registra
--- Proyecto/src/modelo/VO/RecepcionistaVO.py
+++ Proyecto/src/modelo/VO/RecepcionistaVO.py
@@ -1,5 +1,5 @@
 class RecepcionistaVO:
-    def __init__(self, id_recepcionista, id_administrador_registra):
-        self._id_recepcionista = id_recepcionista
-        self._id_administrador_registra = id_administrador_registra
+    def __init__(self, id_recepcionista=None, id_administrador_registra=None):
+        self.id_recepcionista = id_recepcionista
+        self.id_administrador_registra = id_administrador_registra
         
